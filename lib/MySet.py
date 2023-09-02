afac5e6a5484dd41a9597a5fb0b730eb3870d543
KenCorp <kennethwarui35@gmail.com>
2023-09-02 13:37:04 +0300

    Fin

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,32 @@
-class MySet:
+# A Set is a data structure that is used for storing a collection of unique values.
 
-    pass
+# They are useful for problems that involve finding repeated values, or removing duplicate values.
+class MySet:
+  
+  def __init__(self, enumerable = []):
+    self.dictionary = {}
+    for value in enumerable:
+      self.dictionary[value] = True
+
+  def has(self, value):
+    return value in self.dictionary
+  
+  def add(self, value):
+    self.dictionary[value] = True
+    return self
+  
+  def delete(self, value):
+    self.dictionary.pop(value, None)
+    return self
+  
+  def size(self):
+    return len(self.dictionary)
+  
+  def clear(self):
+    self.dictionary.clear()
+    return self
+  
+   
+
+      
+      
